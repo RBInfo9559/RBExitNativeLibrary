cdd902f0dc3554b7d4452a43399a0e07598b3c8e
Solanki Ranvir <solankiranvir217@gmail.com>
2018-04-26 18:02:30 +0530

    Update Commit

---
+++ src/main/java/rb/exit/myexitviewlibrary/AppHelper.java
@@ -0,0 +1,13 @@
+package rb.exit.myexitviewlibrary;
+
+import android.app.Application;
+
+
+public class AppHelper extends Application
+{
+	public static String roboto_font_path = "Roboto-Regular.ttf";
+
+	public static String rate_url = "https://play.google.com/store/apps/details?id=";
+	public static String more_url = "https://play.google.com/store/apps/developer?id=";
+
+}
