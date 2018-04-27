544a0ba6f247ede88b7d82b253a49ed9bf396d08
Solanki Ranvir <solankiranvir217@gmail.com>
2018-04-27 17:40:53 +0530

    Update Commit

--- src/main/java/rb/exit/myexitviewlibrary/MyExitView.java
+++ src/main/java/rb/exit/myexitviewlibrary/MyExitView.java
@@ -4,26 +4,33 @@ package rb.exit.myexitviewlibrary;
 import android.app.Activity;
 import android.app.Application;
 
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Handler;
 
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MyExitView extends Application
 {
     static boolean doubleBackToExitPressedOnce = false;
-
+    private static Context mContext;
 
     public static void init(Context homeActivity)
     {
+        mContext = homeActivity;
         exit_CommonHelper.mActivity = homeActivity;
 
     }
-
-
 
     public static void OpenExitScreen()
     {
@@ -48,9 +55,10 @@ public class MyExitView extends Application
         ((Activity)exit_CommonHelper.mActivity).overridePendingTransition(R.anim.exit_slide_in_left, R.anim.exit_slide_out_right);
     }
 
-    private static void OnBackSelect() {
-
-        if (doubleBackToExitPressedOnce) {
+    private static void OnBackSelect()
+    {
+        if (doubleBackToExitPressedOnce)
+        {
 
             ((Activity)exit_CommonHelper.mActivity).finish();
             ((Activity)exit_CommonHelper.mActivity).overridePendingTransition(R.anim.exit_slide_in_left, R.anim.exit_slide_out_right);
@@ -60,12 +68,126 @@ public class MyExitView extends Application
         doubleBackToExitPressedOnce = true;
         Toast.makeText(exit_CommonHelper.mActivity, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
 
-        new Handler().postDelayed(new Runnable() {
-
+        new Handler().postDelayed(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 doubleBackToExitPressedOnce=false;
             }
         }, 2000);
+    }
+
+    public static void RateApp(Context ctx)
+    {
+        // TODO Auto-generated method stub
+        try
+        {
+            mContext = ctx;
+            String rateUrl = AppHelper.rate_url + mContext.getPackageName();
+
+            String dialog_header = "Rate App";
+            String dialog_message = "Are you sure you want to rate my app?" + "\n" + "Thanks for support!";
+
+            ConformRateDialog(mContext,rateUrl,dialog_header,dialog_message);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void ShareApp(Context ctx)
+    {
+        // TODO Auto-generated method stub
+        try
+        {
+            mContext = ctx;
+            String app_name = mContext.getResources().getString(R.string.app_name) + " :";
+            String shareUrl = AppHelper.rate_url + mContext.getPackageName();
+
+            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, app_name);
+            sharingIntent.putExtra(Intent.EXTRA_TEXT,app_name + "\n" + shareUrl);
+            mContext.startActivity(Intent.createChooser(sharingIntent, "Share via"));
+        }
+        catch (Exception e)
+        {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    private static Dialog conform_dialog;
+    private static Button conform_dialog_btn_yes;
+    private static Button conform_dialog_btn_no;
+
+    private static TextView conform_dialog_txt_header;
+    private static TextView conform_dialog_txt_message;
+
+    private static String conform_dialog_header;
+    private static String conform_dialog_message;
+
+    public static void ConformRateDialog(final Context mContext, final String appUrl, final String header, final String message)
+    {
+        conform_dialog = new Dialog(mContext,R.style.TransparentBackground);
+        conform_dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        conform_dialog.setContentView(R.layout.dialog_conform);
+
+        conform_dialog_btn_yes = (Button) conform_dialog.findViewById(R.id.dialog_conform_btn_yes);
+        conform_dialog_btn_no = (Button) conform_dialog.findViewById(R.id.dialog_conform_btn_no);
+
+        conform_dialog_txt_header = (TextView)conform_dialog.findViewById(R.id.dialog_conform_txt_header);
+        conform_dialog_txt_message = (TextView)conform_dialog.findViewById(R.id.dialog_conform_txt_message);
+
+        Typeface font_type = Typeface.createFromAsset(mContext.getAssets(), AppHelper.roboto_font_path);
+
+        conform_dialog_btn_yes.setTypeface(font_type);
+        conform_dialog_btn_no.setTypeface(font_type);
+
+        conform_dialog_txt_header.setTypeface(font_type);
+        conform_dialog_txt_message.setTypeface(font_type);
+
+        conform_dialog_btn_yes.setText("Rate now");
+        conform_dialog_btn_no.setText("Cancel");
+
+        conform_dialog_header = header;
+        conform_dialog_message = message;
+
+        conform_dialog_txt_header.setText(conform_dialog_header);
+        conform_dialog_txt_message.setText(conform_dialog_message);
+
+        conform_dialog_btn_yes.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Uri uri = Uri.parse(appUrl);
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                try
+                {
+                    mContext.startActivity(goToMarket);
+                }
+                catch (ActivityNotFoundException e)
+                {
+                    e.printStackTrace();
+                }
+
+                conform_dialog.dismiss();
+            }
+        });
+
+        conform_dialog_btn_no.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                conform_dialog.dismiss();
+            }
+        });
+
+        conform_dialog.show();
+
     }
 }
