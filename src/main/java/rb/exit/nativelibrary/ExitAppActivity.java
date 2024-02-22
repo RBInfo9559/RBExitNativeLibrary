c1eedf0ec4e27ced4162abc168363749a5ee5564
rbinfotech <rbinfotech2012@gmail.com>
2024-02-22 15:40:29 +0530

    Version 24 Banner Rect Ad

--- src/main/java/rb/exit/nativelibrary/ExitAppActivity.java
+++ src/main/java/rb/exit/nativelibrary/ExitAppActivity.java
@@ -3,6 +3,7 @@ package rb.exit.nativelibrary;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -118,6 +119,7 @@ public class ExitAppActivity extends Activity
 				boolean check_play_store_user = ExitCommonHelper.is_play_store_install;
 				if(check_play_store_user)
 				{
+					Log.e("ExitLibrary","Ad Load start...");
 					LoadRectangleAd();
 				}
 				else
